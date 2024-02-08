991f749c44fb72d5a2ed29a401645f7690fbf5f6
Mohamed Emara <medoemara104@gmail.com>
2024-02-08 08:23:24 +0200

    Apply max listener limit on bookRouters & borrowerRouters

--- src/app.js
+++ src/app.js
@@ -6,8 +6,34 @@ import bookRouter from './routes/bookRoutes.js';
 import borrowerRouter from "./routes/borrowerRoutes.js";
 import borrowingRouter from "./routes/borrowingRoutes.js";
 import reportsRouter from './routes/reportRoutes.js';
+import rateLimit from 'express-rate-limit';
+
+const limiter = rateLimit({
+    // Here we specify how many requests of the same IP we want to allow in a certain amount of time..
+
+    // We'll make it 10 requests every minute.
+    max: 5,
+    windowMs: 60 * 100,
+    
+    statusCode: 400,
+
+    // message when this limit is exceeded.
+    message: {
+        status: 'fail',
+        message: 'Too many request from this IP!! Please try again in a minute.'
+    }
+});
+
 dotenv.config({ path: '.env' });
 const app = express();
+
+
+
+// Apply max listener limit to prevent abuse on routes from (bookRouter) & (borrowerRouter) 
+app.use('/api/v1/book', limiter);
+app.use('/api/v1/borrower', limiter);
+
+
 
 app.use(express.json());
 app.use(requestLoggerMiddleware);
